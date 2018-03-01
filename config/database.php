753df08e7c8141c47a7d6904771dbe7b05b8b09d
alengere <36770460+alengere@users.noreply.github.com>
2018-03-01 16:22:26 +0100

    Add files via upload

---
+++ config/database.php
@@ -0,0 +1,19 @@
+<?php
+
+
+return [
+'fetch'  => PDO::FETCH_OBJ,
+'driver' => 'mysql',
+'mysql'  => [
+
+	'host'   => '127.0.0.1',
+	'user'	 => 'alen',
+	'pass'	 => '',
+	'db_name'=>'algebra_auth',
+	'charset'=>'utf8'
+
+
+],
+'sqlite' =>[],
+'pgsql'  =>[]
+];
